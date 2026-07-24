f8c5b193a3b20a69c4b98da86e2aebf9688c63a9
Keneth Nolasco <54483710+Kenie4r@users.noreply.github.com>
2026-07-24 15:38:14 -0600

    barra navegacion3

--- desafio1/components/BarraNavegacion.tsx
+++ desafio1/components/BarraNavegacion.tsx
@@ -28,14 +28,17 @@ export default function NavigationBarSS(){
   </a>
 
   <a href="#" className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
-    <svg className="w-6 h-6 mb-2 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg className="w-6 h-6 mb-2 mx-auto" 
+    xmlns="http://www.w3.org/2000/svg"
+     fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
     </svg>
     Profile
   </a>
 
   <a href="#" className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
-    <svg className="w-6 h-6 mb-2 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg className="w-6 h-6 mb-2 mx-auto" xmlns="http://www.w3.org/2000/svg"
+     fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
     </svg>
     Settings
